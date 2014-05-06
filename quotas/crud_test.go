7d10e8617ca71a80ff1ff382c4c545a9287709f9
Tim Jarratt <tjarratt@pivotallabs.com>
2014-05-06 16:34:08 -0700

    Add Quotas CRUD test

---
+++ quotas/crud_test.go
@@ -0,0 +1,37 @@
+package quotas_test
+
+import(
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+	. "github.com/onsi/gomega/gexec"
+	. "github.com/pivotal-cf-experimental/cf-test-helpers/cf"
+)
+
+var _ = Describe("CF Quota commands", func() {
+	It("can Create, Read, Update, and Delete quotas", func() {
+		AsUser(context.AdminUserContext(), func() {
+			Eventually(Cf("create-quota",
+				"quota-name-goes-here",
+				"-m", "512M",
+			), 5.0).Should(Exit(0))
+
+			quotaOutput := Cf("quotas")
+			Eventually(quotaOutput, 5).Should(Say("quota-name-goes-here"))
+
+			Eventually(Cf("update-quota",
+				"quota-name-goes-here",
+				"-m", "513M",
+			), 5).Should(Exit(0))
+
+			Eventually(Cf("quotas")).Should(Say("513M"))
+
+			Eventually(Cf("delete-quota",
+				"quota-name-goes-here",
+				"-f,",
+			)).Should(Exit(0))
+
+			Eventually(Cf("quotas")).ShouldNot(Say("quota-name-goes-here"))
+		})
+	})
+})
