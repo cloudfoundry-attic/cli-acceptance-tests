a8a736edd5ddbc42818b923723db7e1d957b9367
Christopher Piraino and Jonathan Berkhahn <jberkhahn@pivotallabs.com>
2014-08-22 10:08:33 -0700

    Using a string literal seems to work with CATS.
    Why not GATS?

--- helpers/assets.go
+++ helpers/assets.go
@@ -13,13 +13,9 @@ type Assets struct {
 }
 
 func NewAssets() Assets {
-	pwd, err := os.Getwd()
-	if err != nil {
-		panic(err.Error())
-	}
 	return Assets{
-		ServiceBroker:      filepath.FromSlash(path.Join(pwd, "../../assets/service_broker")),
-		SecurityRules:      filepath.FromSlash(path.Join(pwd, "../../assets/security_groups/security-rules.json")),
-		EmptySecurityRules: filepath.FromSlash(path.Join(pwd, "../../assets/security_groups/empty-security-rules.json")),
+		ServiceBroker:      "../../assets/service_broker",
+		SecurityRules:      "../../assets/security_groups/security-rules.json",
+		EmptySecurityRules: "../../assets/security_groups/empty-security-rules.json",
 	}
 }
