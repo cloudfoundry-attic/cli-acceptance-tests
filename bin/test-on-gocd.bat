5de779f386f20913fa4b02d4c7fa8377f8c8bff8
Rasheed Abdul-Aziz <squeedee@gmail.com>
2015-10-02 17:07:11 -0400

    Revert "WIP" - mistaken commit
    This reverts commit 6c474ac81f17abef2a4cf7676a20feb8a1c38b78.

--- bin/test-on-gocd.bat
+++ bin/test-on-gocd.bat
@@ -1,15 +1,15 @@
-DEL c:\Users\Administrator\go\src\github.com\cloudfoundry\cli-acceptance-tests\gcf.exe
-bitsadmin.exe /transfer "DownloadStableCLI" https://s3.amazonaws.com/go-cli/builds/cf-windows-amd64.exe c:\Users\Administrator\go\src\github.com\cloudfoundry\cli-acceptance-tests\cf.exe
+DEL c:\Users\Administrator\go\src\github.com\cloudfoundry\GATS\gcf.exe
+bitsadmin.exe /transfer "DownloadStableCLI" https://s3.amazonaws.com/go-cli/builds/cf-windows-amd64.exe c:\Users\Administrator\go\src\github.com\cloudfoundry\GATS\cf.exe
 
-go get -u github.com/cloudfoundry/cli-acceptance-tests/...
+go get -u github.com/cloudfoundry/GATS/...
 
-SET TESTSPATH=%GOPATH%\src\github.com\cloudfoundry\cli-acceptance-tests
-SET PATH=%PATH%;%TESTSPATH%;C:\Program Files\cURL\bin
+SET GATSPATH=%GOPATH%\src\github.com\cloudfoundry\GATS
+SET PATH=%PATH%;%GATSPATH%;C:\Program Files\cURL\bin
 SET CONFIG=%CD%\config.json
-SET LOCAL_GOPATH=%TESTSPATH%\Godeps\_workspace
+SET LOCAL_GOPATH=%GATSPATH%\Godeps\_workspace
 MKDIR %LOCAL_GOPATH%\bin
 SET GOPATH=%LOCAL_GOPATH%;%GOPATH%
 SET PATH=%LOCAL_GOPATH%\bin;%PATH%
 
 go install -v github.com/onsi/ginkgo/ginkgo
-ginkgo.exe -r -slowSpecThreshold=120 ./cli_tests
+ginkgo.exe -r -slowSpecThreshold=120 ./gats
---
+++ gats/quotas/quotas_suite_test.go
@@ -0,0 +1,30 @@
+package quotas_test
+
+import (
+	"testing"
+
+	"github.com/cloudfoundry-incubator/cf-test-helpers/helpers"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var context helpers.SuiteContext
+
+func TestQuotas(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	config := helpers.LoadConfig()
+	context = helpers.NewContext(config)
+	environment := helpers.NewEnvironment(context)
+
+	BeforeSuite(func() {
+		environment.Setup()
+	})
+
+	AfterSuite(func() {
+		environment.Teardown()
+	})
+
+	RunSpecs(t, "Quotas")
+}
