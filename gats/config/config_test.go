e30deed44bb5b045211d8eef7a48a693c232bc7a
Kris Hicks <khicks@pivotal.io>
2015-11-25 09:26:02 -0800

    Add LANG and LC_ALL tests to gats/config
    [#108574708]

--- gats/config/config_test.go
+++ gats/config/config_test.go
@@ -1,14 +1,17 @@
 package config_test
 
 import (
+	"os"
 	"time"
 
+	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	CATS_helper "github.com/cloudfoundry-incubator/cf-test-helpers/helpers"
 
-	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	. "github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("Config", func() {
@@ -30,70 +33,142 @@ var _ = Describe("Config", func() {
 	It("allows setting locale to de_DE", func() {
 		cf.AsUser(context.AdminUserContext(), setupTimeout, func() {
 			session := cf.Cf("config", "-locale", "de_DE").Wait(commandTimeout)
-			Expect(session).To(Exit(0))
+			Expect(session).To(gexec.Exit(0))
 		})
 	})
 
 	It("allows setting locale to en_US", func() {
 		cf.AsUser(context.AdminUserContext(), setupTimeout, func() {
 			session := cf.Cf("config", "-locale", "en_US").Wait(commandTimeout)
-			Expect(session).To(Exit(0))
+			Expect(session).To(gexec.Exit(0))
 		})
 	})
 
 	It("allows setting locale to es_ES", func() {
 		cf.AsUser(context.AdminUserContext(), setupTimeout, func() {
 			session := cf.Cf("config", "-locale", "es_ES").Wait(commandTimeout)
-			Expect(session).To(Exit(0))
+			Expect(session).To(gexec.Exit(0))
 		})
 	})
 
 	It("allows setting locale to fr_FR", func() {
 		cf.AsUser(context.AdminUserContext(), setupTimeout, func() {
 			session := cf.Cf("config", "-locale", "fr_FR").Wait(commandTimeout)
-			Expect(session).To(Exit(0))
+			Expect(session).To(gexec.Exit(0))
 		})
 	})
 
 	It("allows setting locale to it_IT", func() {
 		cf.AsUser(context.AdminUserContext(), setupTimeout, func() {
 			session := cf.Cf("config", "-locale", "it_IT").Wait(commandTimeout)
-			Expect(session).To(Exit(0))
+			Expect(session).To(gexec.Exit(0))
 		})
 	})
 
 	It("allows setting locale to ja_JA", func() {
 		cf.AsUser(context.AdminUserContext(), setupTimeout, func() {
 			session := cf.Cf("config", "-locale", "ja_JA").Wait(commandTimeout)
-			Expect(session).To(Exit(0))
+			Expect(session).To(gexec.Exit(0))
 		})
 	})
 
 	It("allows setting locale to ko_KR", func() {
 		cf.AsUser(context.AdminUserContext(), setupTimeout, func() {
 			session := cf.Cf("config", "-locale", "ko_KR").Wait(commandTimeout)
-			Expect(session).To(Exit(0))
+			Expect(session).To(gexec.Exit(0))
 		})
 	})
 
 	It("allows setting locale to pt_BR", func() {
 		cf.AsUser(context.AdminUserContext(), setupTimeout, func() {
 			session := cf.Cf("config", "-locale", "pt_BR").Wait(commandTimeout)
-			Expect(session).To(Exit(0))
+			Expect(session).To(gexec.Exit(0))
 		})
 	})
 
 	It("allows setting locale to zh_Hans", func() {
 		cf.AsUser(context.AdminUserContext(), setupTimeout, func() {
 			session := cf.Cf("config", "-locale", "zh_Hans").Wait(commandTimeout)
-			Expect(session).To(Exit(0))
+			Expect(session).To(gexec.Exit(0))
 		})
 	})
 
 	It("allows setting locale to zh_Hant", func() {
 		cf.AsUser(context.AdminUserContext(), setupTimeout, func() {
 			session := cf.Cf("config", "-locale", "zh_Hant").Wait(commandTimeout)
-			Expect(session).To(Exit(0))
+			Expect(session).To(gexec.Exit(0))
+		})
+	})
+
+	Context("when LANG is set", func() {
+		var origLANG string
+
+		BeforeEach(func() {
+			origLANG = os.Getenv("LANG")
+			os.Setenv("LANG", "fr_FR")
+		})
+
+		AfterEach(func() {
+			os.Setenv("LANG", origLANG)
+		})
+
+		It("uses LANG to load the translations", func() {
+			cf.AsUser(context.AdminUserContext(), setupTimeout, func() {
+				session := cf.Cf("help").Wait(commandTimeout)
+				Expect(session).To(gexec.Exit(0))
+				Expect(session).To(gbytes.Say("UTILISATION"))
+			})
+		})
+
+		It("uses the locale to load the translations when a locale is set", func() {
+			cf.AsUser(context.AdminUserContext(), setupTimeout, func() {
+				session := cf.Cf("config", "-locale", "es_ES").Wait(commandTimeout)
+				Expect(session).To(gexec.Exit(0))
+				session = cf.Cf("help").Wait(commandTimeout)
+				Expect(session).To(gexec.Exit(0))
+				Expect(session).To(gbytes.Say("USO"))
+			})
+		})
+	})
+
+	Context("when LC_ALL is set", func() {
+		var origLCALL string
+
+		BeforeEach(func() {
+			origLCALL = os.Getenv("LC_ALL")
+			os.Setenv("LC_ALL", "fr_FR")
+		})
+
+		AfterEach(func() {
+			os.Setenv("LC_ALL", origLCALL)
+		})
+
+		It("uses LC_ALL to load the translations", func() {
+			cf.AsUser(context.AdminUserContext(), setupTimeout, func() {
+				session := cf.Cf("help").Wait(commandTimeout)
+				Expect(session).To(gexec.Exit(0))
+				Expect(session).To(gbytes.Say("UTILISATION"))
+			})
+		})
+
+		It("uses the locale to load the translations when a locale is set", func() {
+			cf.AsUser(context.AdminUserContext(), setupTimeout, func() {
+				session := cf.Cf("config", "-locale", "es_ES").Wait(commandTimeout)
+				Expect(session).To(gexec.Exit(0))
+				session = cf.Cf("help").Wait(commandTimeout)
+				Expect(session).To(gexec.Exit(0))
+				Expect(session).To(gbytes.Say("USO"))
+			})
+		})
+	})
+
+	Context("when LANG, LC_ALL, and locale are not set", func() {
+		It("defaults to en_US", func() {
+			cf.AsUser(context.AdminUserContext(), setupTimeout, func() {
+				session := cf.Cf("help").Wait(commandTimeout)
+				Expect(session).To(gexec.Exit(0))
+				Expect(session).To(gbytes.Say("USAGE"))
+			})
 		})
 	})
 })
