b655e8744a729f6dbb8a2d510181316e38fbc6fc
Nick Wei <nwei@pivotal.io>
2016-07-21 15:02:17 -0700

    integration test for verbose output with CF_TRACE=true
    [#126256605]
    
    Signed-off-by: Corbin Halliwill <challiwill@pivotal.io>

--- integration/verbose_flag_test.go
+++ integration/verbose_flag_test.go
@@ -1,6 +1,7 @@
 package integration
 
 import (
+	"os"
 	"os/exec"
 
 	. "github.com/onsi/ginkgo"
@@ -41,4 +42,43 @@ var _ = Describe("Verbose", func() {
 			Eventually(session).Should(Say("RESPONSE:"))
 		})
 	})
+
+	Context("when the CF_TRACE env variable is set", func() {
+		var (
+			commandErr error
+			session    *Session
+		)
+
+		BeforeEach(func() {
+			login := exec.Command("cf", "auth", "admin", "admin")
+			loginSession, err := Start(login, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(loginSession).Should(Exit(0))
+
+			err = os.Setenv("CF_TRACE", "true")
+			Expect(err).NotTo(HaveOccurred())
+
+			command := exec.Command("cf", "orgs")
+			session, commandErr = Start(command, GinkgoWriter, GinkgoWriter)
+		})
+
+		AfterEach(func() {
+			err := os.Setenv("CF_TRACE", "")
+			Expect(err).NotTo(HaveOccurred())
+
+			logout := exec.Command("cf", "logout")
+			logoutSession, err := Start(logout, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(logoutSession).Should(Exit(0))
+		})
+
+		It("displays verbose output", func() {
+			Expect(commandErr).NotTo(HaveOccurred())
+			Eventually(session).Should(Exit(0))
+			Eventually(session).Should(Say("REQUEST:"))
+			Eventually(session).Should(Say("GET /v2/organizations"))
+			Eventually(session).Should(Say("RESPONSE:"))
+		})
+	})
+
 })
