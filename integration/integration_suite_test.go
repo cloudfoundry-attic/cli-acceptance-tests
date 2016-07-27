3fb9cc898390aa7a7f2a23535c8d48269702d85c
Nick Wei <nwei@pivotal.io>
2016-07-27 11:12:54 -0700

    set the api for the entire test suite
    [#126256605]
    
    Signed-off-by: Jeff Pak <jeffrey.pak@emc.com>

--- integration/integration_suite_test.go
+++ integration/integration_suite_test.go
@@ -1,13 +1,24 @@
 package integration
 
 import (
+	"os/exec"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
 
 	"testing"
 )
 
 func TestIntegration(t *testing.T) {
 	RegisterFailHandler(Fail)
+	setAPI()
 	RunSpecs(t, "Integration Suite")
+}
+
+func setAPI() {
+	api := exec.Command("cf", "api", "api.bosh-lite.com", "--skip-ssl-validation")
+	apiSession, err := Start(api, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(apiSession).Should(Exit(0))
 }
