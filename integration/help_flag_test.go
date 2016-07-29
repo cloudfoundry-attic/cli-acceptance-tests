f31809014007c9d51cc846c967ec0dce899cde72
Anand Gaitonde <agaitonde@pivotal.io>
2016-07-29 14:41:38 -0700

    test refactor

--- integration/help_flag_test.go
+++ integration/help_flag_test.go
@@ -4,76 +4,52 @@ import (
 	"os/exec"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gbytes"
 	. "github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("Help Flag", func() {
-	Context("when cf is run without providing a command or a flag", func() {
-		var (
-			commandErr error
-			session    *Session
-		)
+	DescribeTable("displays the help text",
+		func(setup func() *exec.Cmd) {
+			cmd := setup()
+			session, err := Start(cmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
 
-		BeforeEach(func() {
-			command := exec.Command("cf")
-			session, commandErr = Start(command, GinkgoWriter, GinkgoWriter)
-		})
-
-		It("displays the help text", func() {
-			Expect(commandErr).NotTo(HaveOccurred())
-			Eventually(session).Should(Exit(0))
 			Eventually(session).Should(Say("NAME:"))
 			Eventually(session).Should(Say("USAGE:"))
 			Eventually(session).Should(Say("VERSION:"))
 			Eventually(session).Should(Say("GETTING STARTED:"))
 			Eventually(session).Should(Say("ENVIRONMENT VARIABLES:"))
 			Eventually(session).Should(Say("GLOBAL OPTIONS:"))
-		})
-	})
-
-	Context("when cf is run with -h flag alone", func() {
-		var (
-			commandErr error
-			session    *Session
-		)
-
-		BeforeEach(func() {
-			command := exec.Command("cf", "-h")
-			session, commandErr = Start(command, GinkgoWriter, GinkgoWriter)
-		})
-
-		It("displays the help text", func() {
-			Expect(commandErr).NotTo(HaveOccurred())
 			Eventually(session).Should(Exit(0))
-			Eventually(session).Should(Say("NAME:"))
-			Eventually(session).Should(Say("USAGE:"))
-			Eventually(session).Should(Say("VERSION:"))
-			Eventually(session).Should(Say("GETTING STARTED:"))
-			Eventually(session).Should(Say("ENVIRONMENT VARIABLES:"))
-			Eventually(session).Should(Say("GLOBAL OPTIONS:"))
-		})
-	})
+		},
+
+		Entry("when cf is run without providing a command or a flag", func() *exec.Cmd {
+			return exec.Command("cf")
+		}),
+
+		Entry("when cf is run with -h flag alone", func() *exec.Cmd {
+			return exec.Command("cf", "-h")
+		}),
+
+		Entry("when cf is run with --help flag alone", func() *exec.Cmd {
+			return exec.Command("cf", "--help")
+		}),
+	)
 
 	Context("when cf is run with -h flag on a command", func() {
-		var (
-			commandErr error
-			session    *Session
-		)
-
-		BeforeEach(func() {
-			command := exec.Command("cf", "app", "-h")
-			session, commandErr = Start(command, GinkgoWriter, GinkgoWriter)
-		})
-
 		It("displays the help text", func() {
-			Expect(commandErr).NotTo(HaveOccurred())
-			Eventually(session).Should(Exit(0))
+			command := exec.Command("cf", "app", "-h")
+			session, err := Start(command, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+
 			Eventually(session).Should(Say("NAME:"))
 			Eventually(session).Should(Say("app - Display health and status for ap"))
 			Eventually(session).Should(Say("USAGE:"))
 			Eventually(session).Should(Say("OPTIONS:"))
+			Eventually(session).Should(Exit(0))
 		})
 	})
 })
