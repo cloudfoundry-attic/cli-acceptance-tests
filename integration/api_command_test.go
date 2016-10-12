155cfb4a8442e690d13e7569a6b5e360baeb3ad4
Matthew Boedicker <mboedicker@pivotal.io>
2016-10-12 08:58:23 -0700

    utils/config was moved to utils/configv3

--- integration/api_command_test.go
+++ integration/api_command_test.go
@@ -9,7 +9,7 @@ import (
 	"path/filepath"
 	"strings"
 
-	"code.cloudfoundry.org/cli/utils/config"
+	"code.cloudfoundry.org/cli/utils/configv3"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -50,20 +50,20 @@ var _ = Describe("API Command", func() {
 					org = "the-org"
 					space = "the-space"
 
-					userConfig := config.Config{
-						ConfigFile: config.CFConfig{
+					userConfig := configv3.Config{
+						ConfigFile: configv3.CFConfig{
 							Target:      target,
 							APIVersion:  apiVersion,
 							AccessToken: "bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImxlZ2FjeS10b2tlbi1rZXkiLCJ0eXAiOiJKV1QifQ.eyJqdGkiOiI3YzZkMDA2MjA2OTI0NmViYWI0ZjBmZjY3NGQ3Zjk4OSIsInN1YiI6Ijk1MTliZTNlLTQ0ZDktNDBkMC1hYjlhLWY0YWNlMTFkZjE1OSIsInNjb3BlIjpbIm9wZW5pZCIsInJvdXRpbmcucm91dGVyX2dyb3Vwcy53cml0ZSIsInNjaW0ucmVhZCIsImNsb3VkX2NvbnRyb2xsZXIuYWRtaW4iLCJ1YWEudXNlciIsInJvdXRpbmcucm91dGVyX2dyb3Vwcy5yZWFkIiwiY2xvdWRfY29udHJvbGxlci5yZWFkIiwicGFzc3dvcmQud3JpdGUiLCJjbG91ZF9jb250cm9sbGVyLndyaXRlIiwiZG9wcGxlci5maXJlaG9zZSIsInNjaW0ud3JpdGUiXSwiY2xpZW50X2lkIjoiY2YiLCJjaWQiOiJjZiIsImF6cCI6ImNmIiwiZ3JhbnRfdHlwZSI6InBhc3N3b3JkIiwidXNlcl9pZCI6Ijk1MTliZTNlLTQ0ZDktNDBkMC1hYjlhLWY0YWNlMTFkZjE1OSIsIm9yaWdpbiI6InVhYSIsInVzZXJfbmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbiIsImF1dGhfdGltZSI6MTQ3MzI4NDU3NywicmV2X3NpZyI6IjZiMjdkYTZjIiwiaWF0IjoxNDczMjg0NTc3LCJleHAiOjE0NzMyODUxNzcsImlzcyI6Imh0dHBzOi8vdWFhLmJvc2gtbGl0ZS5jb20vb2F1dGgvdG9rZW4iLCJ6aWQiOiJ1YWEiLCJhdWQiOlsiY2YiLCJvcGVuaWQiLCJyb3V0aW5nLnJvdXRlcl9ncm91cHMiLCJzY2ltIiwiY2xvdWRfY29udHJvbGxlciIsInVhYSIsInBhc3N3b3JkIiwiZG9wcGxlciJdfQ.OcH_w9yIKJkEcTZMThIs-qJAHk3G0JwNjG-aomVH9hKye4ciFO6IMQMLKmCBrrAQVc7ST1SZZwq7gv12Dq__6Jp-hai0a2_ADJK-Vc9YXyNZKgYTWIeVNGM1JGdHgFSrBR2Lz7IIrH9HqeN8plrKV5HzU8uI9LL4lyOCjbXJ9cM",
-							TargetedOrganization: config.Organization{
+							TargetedOrganization: configv3.Organization{
 								Name: org,
 							},
-							TargetedSpace: config.Space{
+							TargetedSpace: configv3.Space{
 								Name: space,
 							},
 						},
 					}
-					err := config.WriteConfig(&userConfig)
+					err := configv3.WriteConfig(&userConfig)
 					Expect(err).ToNot(HaveOccurred())
 				})
 
@@ -100,21 +100,21 @@ var _ = Describe("API Command", func() {
 		Context("--unset is passed", func() {
 			BeforeEach(func() {
 
-				userConfig := config.Config{
-					ConfigFile: config.CFConfig{
+				userConfig := configv3.Config{
+					ConfigFile: configv3.CFConfig{
 						ConfigVersion: 3,
 						Target:        "https://api.fake.com",
 						APIVersion:    "2.59.0",
 						AccessToken:   "bearer tokenstuff",
-						TargetedOrganization: config.Organization{
+						TargetedOrganization: configv3.Organization{
 							Name: "the-org",
 						},
-						TargetedSpace: config.Space{
+						TargetedSpace: configv3.Space{
 							Name: "the-space",
 						},
 					},
 				}
-				err := config.WriteConfig(&userConfig)
+				err := configv3.WriteConfig(&userConfig)
 				Expect(err).ToNot(HaveOccurred())
 			})
 
@@ -130,7 +130,7 @@ var _ = Describe("API Command", func() {
 				rawConfig, err := ioutil.ReadFile(filepath.Join(homeDir, ".cf", "config.json"))
 				Expect(err).NotTo(HaveOccurred())
 
-				var configFile config.CFConfig
+				var configFile configv3.CFConfig
 				err = json.Unmarshal(rawConfig, &configFile)
 				Expect(err).NotTo(HaveOccurred())
 
@@ -244,7 +244,7 @@ var _ = Describe("API Command", func() {
 			rawConfig, err := ioutil.ReadFile(filepath.Join(homeDir, ".cf", "config.json"))
 			Expect(err).NotTo(HaveOccurred())
 
-			var configFile config.CFConfig
+			var configFile configv3.CFConfig
 			err = json.Unmarshal(rawConfig, &configFile)
 			Expect(err).NotTo(HaveOccurred())
 
@@ -279,7 +279,7 @@ var _ = Describe("API Command", func() {
 			rawConfig, err := ioutil.ReadFile(filepath.Join(homeDir, ".cf", "config.json"))
 			Expect(err).NotTo(HaveOccurred())
 
-			var configFile config.CFConfig
+			var configFile configv3.CFConfig
 			err = json.Unmarshal(rawConfig, &configFile)
 			Expect(err).NotTo(HaveOccurred())
 
@@ -296,7 +296,7 @@ var _ = Describe("API Command", func() {
 		rawConfig, err := ioutil.ReadFile(filepath.Join(homeDir, ".cf", "config.json"))
 		Expect(err).NotTo(HaveOccurred())
 
-		var configFile config.CFConfig
+		var configFile configv3.CFConfig
 		err = json.Unmarshal(rawConfig, &configFile)
 		Expect(err).NotTo(HaveOccurred())
 
